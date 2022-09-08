8a5e5bfff615f36f8022ede278dbd20c6e82e42e
Skluad <skluad@Sartaj.local>
2022-09-08 13:52:59 +0530

    added home

---
+++ client/src/component/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+
+
+
+
+    const renderHome=()=>{
+        
+    }
+
+
+
+
+
+  return (
+    <div>
+        {renderHome()}
+    </div>
+  )
+}
+
+export default Home
